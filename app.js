bbfaca6040204274aafcd7758235bef3a59e88c0
mahdiyousefidev <mahdiyousefi.developer@gmail.com>
2020-01-15 17:39:30 +0330

    provide service due to url

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http');
-
+const fs=require('fs');
 const server = http.createServer((req,res)=>{
-console.log(req.url, req.method,req.headers);
-// process.exit();
-res.setHeader('Content-Type','text/html');
+if(req.url==='/'){
+    res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>Send Message</title></head>');
+res.write('<body><form action="/message" method="POST"><spna>Enter your message here:</span><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+res.write('<html>');
+return res.end();
+}
+if(req.url === '/message' && req.method === 'POST'){
+fs.writeFileSync('message.txt','Yummy mummy');
+res.statusCode=302;
+res.setHeader('Location','/');
+return res.end();
+}
+    res.setHeader('Content-Type','text/html');
 res.write('<html>');
 res.write('<head><title>My First Page</title></head>');
 res.write('<body><h1>Hello from Node.js Server!</h1></body>');
